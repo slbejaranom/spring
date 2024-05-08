f2da4ecf2338fed35535fe1fcfb1efbe80cd0516
Sebastian Bejarano <slbejaranom@gmail.com>
2024-05-08 14:29:02 -0500

    Saludar hecho

--- src/main/java/ejemplo/spring/miapp/controller/MiPrimerControlador.java
+++ src/main/java/ejemplo/spring/miapp/controller/MiPrimerControlador.java
@@ -2,6 +2,7 @@ package ejemplo.spring.miapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,12 @@ public class MiPrimerControlador {
   @GetMapping("/hola-mundo")
   public String handleGetHolaMundo() {
     return "Hola mundo";
+  }
+
+  @GetMapping("/saludar")
+  public String handleSaludar(@RequestParam String nombre) {
+    StringBuilder stringBuilder = new StringBuilder("Hola ");
+    stringBuilder.append(nombre);
+    return stringBuilder.toString();
   }
 }
